59f0228dca16b6782fa8a15a8ef365dc1f1114b1
nth@terminal <nth@terminal.hell>
2023-10-10 00:15:31 +0800

    dirty fix someone please do a better pr :pray:

--- src/lib/blob.svelte
+++ src/lib/blob.svelte
@@ -1,8 +1,11 @@
 <!-- todo put this in a better place, change path on +layout.svelte thank you / nk -->
+<!-- too dumb to sort mouse movements please someone make a better PR thanks / nk -->
 <script>
 	import { onMount } from "svelte";
 
     let blob;
+    let t = 0;
+    
     const mm = event => { 
         const { clientX, clientY } = event;
 
@@ -12,8 +15,7 @@
         }, { duration: 1000, fill: "forwards" });
     }
 
-
-    function updateGradient(t) {
+    const update = t => {
         const width = blob.clientWidth;
         const height = blob.clientHeight;
 
@@ -25,17 +27,16 @@
         blob.style.transform = `translate(-50%, -50%) rotate(${(t / 100) % 360}deg)`
     }
 
-    // Call updateGradient with your desired t value, e.g., every frame
-    let t = 0;
-    function animate() {
-        updateGradient(t);
-        t += 16; // Increment t with a suitable value to control the speed of the animation (e.g., 16ms for 60fps)
+    const animate = () => {
+        update(t);
+        t += 16;
         requestAnimationFrame(animate);
     }
     onMount(animate);
 </script>
 
 <main class="w-screen h-screen bg-transparent absolute" on:mousemove={mm}> <!-- as much as i would to z-index this behind itll disable the mouse effect! -->
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
     <div id="blob" bind:this={blob} class="blob absolute -z-10 top-1/2 left-1/2 pointer-events-auto rounded-[50%] bg-white h-[80vh] w-[50vw] blur-[200rem]">
 
     </div>
