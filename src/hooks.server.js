03e2582f8c8b498d06648f9cc63197ff23b7be5e
CelestialCrafter <celestialcraftermc@gmail.com>
2023-10-09 03:50:26 -0500

    provide account data to dashboard

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -15,7 +15,7 @@ connect(DB_URI);
 const redis = createClient();
 
 // Algorithm Check Job
-schedule.scheduleJob('*/5 * * * *', async () => {
+schedule.scheduleJob('*/1 * * * *', async () => {
 	console.log('Running algorithm check');
 
 	try {
