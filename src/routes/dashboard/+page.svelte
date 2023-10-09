f2662131fb8bf8f2eac4672add7e0d0d2aed3cca
nth@terminal <nth@terminal.hell>
2023-10-10 01:35:12 +0800

    dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,12 +1,14 @@
 <script>
 	import { enhance } from '$app/forms';
+	import Blob from '../../lib/blob.svelte';
 	import Graph from './Graph.svelte';
 
+	import Sidebar from './Sidebar.svelte';
 	export let data;
 </script>
 
-<main>
-	<form method="post" use:enhance>
+<main class="p-4 h-screen">
+	<!-- reference: <form method="post" use:enhance>
 		<input type="text" name="id" placeholder="ID" />
 		<input type="number" name="strengthToUSD" placeholder="1.0 Strength -> USD" />
 		<input type="text" name="algorithm" placeholder="Algorithm" /><br />
@@ -24,16 +26,18 @@
 		<Graph token={data.token} path={`worth/${id}`} />
 	{/each}
 
-	<code>{data.data}</code>
+	<code>{data.data}</code> -->
+	<Blob/>
+	<div class="border w-full h-full bg-gray-950/90 border-gray-500 rounded-md flex p-3 gap-3">
+		<Sidebar data={data}/>
+		<div class="border w-full h-full bg-gray-950/20 border-gray-500/20 rounded-md flex p-3 focus-within:border-gray-500/80 flex-col gap-3 justify-center items-center">
+			<h1 class="text-4xl opacity-50">graph placeholder</h1>
+			<img class="opacity-25" src="https://media.discordapp.net/attachments/1117102068851806309/1160867501140365342/scugclap.gif?ex=6536391c&is=6523c41c&hm=3e5ecbcb66eb983a8635205bf76df2873b960d91a0f88d4a6798df53997f1125&">
+		</div>	
+	</div>
 </main>
 
 <style>
-	.balance {
-		color: #4e4e4e;
-		font-size: 16px;
-		font-style: italic;
-	}
-
 	.private {
 		user-select: none;
 		color: #3e3e3e;
---
+++ src/routes/dashboard/Sidebar.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let data;
+</script>
+<main>
+    <div class="w-[350px] border h-full bg-gray-950/30 border-gray-500/20 rounded-md p-3 flex flex-col gap-2">
+        <div class="flex gap-2">
+            <input type="text" class="w-full h-16 border bg-gray-950/30 border-gray-500/40 rounded-md p-3 flex flex-col gap-2" placeholder="search">
+            <button formaction="/bots?/create" class="h-16 w-[70px] text-2xl hover:bg-green-500 hover:text-black transition-all">+</button>
+        </div>
+        {#each data.accounts as { address, balance }}
+            <div class="border w-full bg-gray-950/90 border-gray-500/40 p-3 overflow-hidden whitespace-nowrap border-l-green-500 border-l-4">
+                <h1 class="text-2xl text-ellipsis overflow-hidden whitespace-nowrap">{address}</h1>
+                <span class="balance">Net Worth: {balance} USD</span><br />
+            </div>	
+        {/each}
+    </div>
+</main>
+
+<style>
+    .balance {
+		color: #4e4e4e;
+		font-size: 16px;
+		font-style: italic;
+	}
+    input::placeholder {
+        color: #4e4e4e;
+        font-style: italic;
+    }
+</style>
