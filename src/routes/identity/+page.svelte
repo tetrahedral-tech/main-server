34a92bf161bfa93d8acd2c554d38709c44da7722
nth@terminal <nth@terminal.hell>
2023-10-10 00:15:31 +0800

    dirty fix someone please do a better pr :pray:

--- src/routes/identity/+page.svelte
+++ src/routes/identity/+page.svelte
@@ -6,13 +6,13 @@
 
 <main class="w-screen h-screen flex justify-center items-center">
 	<Blob/>
-	<div class="flex flex-col w-1/5 h-4/5 bg-gray-950/80 border-gray-700 border overflow-hidden border-solid rounded-md pointer-events-none">
+	<div class="flex flex-col w-1/5 h-4/5 bg-gray-950/80 border-gray-700 border overflow-hidden border-solid rounded-md">
 		<div class="p-6 bg-gray-900">
 			<h1 class="text-xl">login!</h1>
 		</div>
-		<div class="flex justify-center items-center flex-col h-full">
-			<GoogleButton/>
-			<DiscordButton/>
+		<div class="flex justify-center items-center flex-col h-full gap-2 flex-grow">
+			<!-- <GoogleButton/> -->
+			<!-- <DiscordButton/> -->
 		</div>
 	</div>
 </main>
