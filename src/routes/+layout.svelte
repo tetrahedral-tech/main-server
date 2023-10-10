c4fc39edfb371e7834be00aa78f1ec6e33068724
nth@terminal <nth@terminal.hell>
2023-10-10 22:32:20 +0800

    added most functionality back

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
 	import './styles.css';
+	import Blob from '../lib/components/Blob.svelte';
 </script>
 
+<Blob/>
 <slot />
--- src/routes/dashboard/+layout.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	import Blob from '$lib/components/Blob.svelte';
-</script>
-
-<Blob />
-<slot />
