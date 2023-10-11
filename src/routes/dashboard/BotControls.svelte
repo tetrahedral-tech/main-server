3c81743b88fd5654e1fe1e9ed55c6ea7fb0497da
CelestialCrafter <celestialcraftermc@gmail.com>
2023-10-11 05:08:53 -0500

    add error page add force login

--- src/routes/dashboard/BotControls.svelte
+++ src/routes/dashboard/BotControls.svelte
@@ -3,13 +3,25 @@
 	export let account;
 </script>
 
-<section class={`
-	${account ? "opacity-100 pointer-events-auto" : "opacity-20 pointer-events-none" }
-	bg-gray-950/40 justify-center items-center flex
-`}>
-	<button class="square hover:bg-white" title="Temporary Pause Bot"><Clock class="h-6 w-6" /></button>
-	<button class="square hover:bg-green-500 border-green-500/20" title="Resume Bot"><Play class="h-6 w-6" /></button>
-	<button class="square hover:bg-yellow-500 border-yellow-500/20" title="Pause Bot"><Pause class="h-6 w-6" /></button>
-	<button class="square hover:bg-rose-500 border-rose-500/20" title="Delete Bot"><Minus class="h-6 w-6" /></button>
-	<button class="square hover:bg-purple-500 border-purple-500/20"> <AdjustmentsHorizontal class="h-6 w-6" /></button>
+<section
+	class="
+	{account ? 'opacity-100 pointer-events-auto' : 'opacity-20 pointer-events-none'}
+	transition-[opacity] duration-300 bg-gray-950/40 justify-center items-center flex
+"
+>
+	<button class="square hover:bg-white" title="Temporary Pause Bot"
+		><Clock class="h-6 w-6" /></button
+	>
+	<button class="square hover:bg-green-500 border-green-500/20" title="Resume Bot"
+		><Play class="h-6 w-6" /></button
+	>
+	<button class="square hover:bg-yellow-500 border-yellow-500/20" title="Pause Bot"
+		><Pause class="h-6 w-6" /></button
+	>
+	<button class="square hover:bg-rose-500 border-rose-500/20" title="Delete Bot"
+		><Minus class="h-6 w-6" /></button
+	>
+	<button class="square hover:bg-purple-500 border-purple-500/20">
+		<AdjustmentsHorizontal class="h-6 w-6" /></button
+	>
 </section>
