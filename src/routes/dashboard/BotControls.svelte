3df50a9f6938cecb40a04d1bfc289e1580f80688
nth@terminal <nth@terminal.hell>
2023-10-11 17:20:03 +0800

    colorize bot controls

--- src/routes/dashboard/BotControls.svelte
+++ src/routes/dashboard/BotControls.svelte
@@ -1,11 +1,14 @@
 <script>
 	import { Pause, Play, Clock, Minus, AdjustmentsHorizontal } from 'svelte-heros-v2';
+	export let account;
 </script>
 
-<section>
-	<button class="square" title="Pause Bot"><Pause class="h-6 w-6" /></button>
-	<button class="square" title="Resume Bot"><Play class="h-6 w-6" /></button>
-	<button class="square" title="Temporary Pause Bot"><Clock class="h-6 w-6" /></button>
-	<button class="square" title="Delete Bot"><Minus class="h-6 w-6" /></button>
-	<button class="square"><AdjustmentsHorizontal class="h-6 w-6" /></button>
+<section class={`
+	${account ? "opacity-100 pointer-events-auto" : "opacity-20 pointer-events-none" }
+`}>
+	<button class="square hover:bg-white" title="Temporary Pause Bot"><Clock class="h-6 w-6" /></button>
+	<button class="square hover:bg-green-500 border-green-500/20" title="Resume Bot"><Play class="h-6 w-6" /></button>
+	<button class="square hover:bg-yellow-500 border-yellow-500/20" title="Pause Bot"><Pause class="h-6 w-6" /></button>
+	<button class="square hover:bg-rose-500 border-rose-500/20" title="Delete Bot"><Minus class="h-6 w-6" /></button>
+	<button class="square hover:bg-purple-500 border-purple-500/20"> <AdjustmentsHorizontal class="h-6 w-6" /></button>
 </section>
