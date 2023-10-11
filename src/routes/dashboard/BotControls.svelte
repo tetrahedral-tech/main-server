eb33610b57acedfddb536084ab5cbfaa410dd01e
CelestialCrafter <celestialcraftermc@gmail.com>
2023-10-11 06:40:49 -0500

    fix button.square hover overriding inline hover

--- src/routes/dashboard/BotControls.svelte
+++ src/routes/dashboard/BotControls.svelte
@@ -9,17 +9,15 @@
 	bg-section-200 flex items-center justify-center transition-[opacity] duration-300
 "
 >
-	<button class="square hover:bg-white" title="Temporary Pause Bot"><Clock class="icon" /></button>
-	<button class="square border-green-500/20 hover:bg-green-500" title="Resume Bot"
-		><Play class="icon" /></button
-	>
-	<button class="square border-yellow-500/20 hover:bg-yellow-500" title="Pause Bot"
-		><Pause class="icon" /></button
-	>
-	<button class="square border-rose-500/20 hover:bg-rose-500" title="Delete Bot"
-		><Minus class="icon" /></button
-	>
-	<button class="square hover:bg-accent border-accent/20">
-		<AdjustmentsHorizontal class="icon" /></button
-	>
+	<button class="square hover:!bg-white" title="Temporary Pause Bot"><Clock class="icon" /></button>
+	<button class="square border-green-500/20 hover:!bg-green-500" title="Resume Bot"
+		><Play class="icon" />
+	</button>
+	<button class="square border-yellow-500/20 hover:!bg-yellow-500" title="Pause Bot"
+		><Pause class="icon" />
+	</button>
+	<button class="square border-rose-500/20 hover:!bg-rose-500" title="Delete Bot">
+		<Minus class="icon" />
+	</button>
+	<button class=" border-accent/20 square"><AdjustmentsHorizontal class="icon" /></button>
 </section>
