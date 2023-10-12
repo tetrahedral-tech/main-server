a01a310fa827db4a0fcc263f6088def6c88b2cff
CelestialCrafter <celestialcraftermc@gmail.com>
2023-10-12 04:00:02 -0500

    force graph to rerender on prop change

--- src/lib/components/Graph.svelte
+++ src/lib/components/Graph.svelte
@@ -1,17 +1,19 @@
 <script>
 	import { PUBLIC_ALGORITHM_SERVER_BASE_URL } from '$env/static/public';
-	import { onMount } from 'svelte';
 
 	export let token;
 	export let path;
 	let graph;
 
-	onMount(() => {
+	const loadGraph = () => {
 		fetch(`${PUBLIC_ALGORITHM_SERVER_BASE_URL}/${path}`, { headers: { Authorization: token } })
 			.then(response => response.blob())
 			.then(data => (graph.src = window.URL.createObjectURL(data)))
 			.catch(() => (graph.alt = 'Failed to load graph.'));
-	});
+	};
 </script>
 
-<img {...$$restProps} alt="Graph" bind:this={graph} />
+{#key (token, path)}
+	{loadGraph() || ''}
+	<img {...$$restProps} alt="Graph" bind:this={graph} />
+{/key}
