0405943388e5639524efb222f023961837a21d5b
CelestialCrafter <celestialcraftermc@gmail.com>
2023-10-12 15:50:02 -0500

    partially implement statuses

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -35,18 +35,30 @@ schedule.scheduleJob('*/5 * * * *', async () => {
 	const strengths = await redis.hGetAll('strengths');
 	const bots = await Bot.find({}).populate('algorithm');
 
-	const tradeData = bots.map(bot => {
-		const field = bot.algorithm.owner ? bot.algorithm._id : bot.algorithm.name;
-		const signal = signals[field];
-		const strength = Number(strengths[field]);
+	const tradeData = bots
+		.map(bot => {
+			if (bot.state.type === 'paused') return;
+			if (bot.state.type === 'tempPaused')
+				if (bot.state.time > Date.now()) {
+					/* eslint-disable no-param-reassign */
+					bot.state.type = 'running';
+					bot.state.time = null;
+					/* eslint-disable no-param-reassign */
+					Bot.save();
+				} else return;
 
-		return {
-			id: bot._id,
-			privateKey: bot.privateKey,
-			amount: bot.strengthToUSD * strength,
-			signal
-		};
-	});
+			const field = bot.algorithm.owner ? bot.algorithm._id : bot.algorithm.name;
+			const signal = signals[field];
+			const strength = Number(strengths[field]);
+
+			return {
+				id: bot._id,
+				privateKey: bot.privateKey,
+				amount: bot.strengthToUSD * strength,
+				signal
+			};
+		})
+		.filter(b => b);
 
 	await executeTransactions(tradeData);
 
