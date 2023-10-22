dcf93879d7c75ae0902c096920e6b5a6c3bc17c8
CelestialCrafter <celestialcraftermc@gmail.com>
2023-10-21 23:34:33 -0500

    fixed race conditions in db added more stuff on the bot states

--- src/lib/models.server.js
+++ src/lib/models.server.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
-const { Schema, model, models, plugin } = mongoose;
+
+const { Schema, model, models, plugin, connection } = mongoose;
 
 // Define a global plugin that makes all fields required (unless they already have a requiremenet specifically set)
 const intoRequired = schema =>
@@ -8,71 +9,93 @@ const intoRequired = schema =>
 	);
 plugin(intoRequired);
 
-const identitySchema = Schema({
-	provider: String,
-	name: String,
-	photo: String,
-	gender: {
-		type: String,
-		required: false
-	},
-	_id: String
-});
-
-const userSchema = Schema({
-	admin: Boolean,
-	identity: {
-		type: String,
-		ref: 'Identity'
-	}
-});
-
-const algorithmSchema = Schema({
-	name: String,
-	owner: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-		required: false
-	}
-});
-
-const botSchema = Schema(
-	{
+const schemas = {
+	Identity: Schema({
+		provider: String,
+		name: String,
+		photo: String,
+		gender: {
+			type: String,
+			required: false
+		},
+		_id: String
+	}),
+	User: Schema({
+		admin: Boolean,
+		identity: {
+			type: String,
+			ref: 'Identity'
+		}
+	}),
+	Algorithm: Schema({
+		name: String,
 		owner: {
 			type: Schema.Types.ObjectId,
-			ref: 'User'
-		},
-		algorithm: {
-			type: Schema.Types.ObjectId,
-			ref: 'Algorithm'
-		},
-		strengthToUSD: Number,
-		encryptedPrivateKey: String,
-		worth: [
-			{
-				timestamp: Number,
-				value: Number
+			ref: 'User',
+			required: false
+		}
+	}),
+	Bot: Schema(
+		{
+			owner: {
+				type: Schema.Types.ObjectId,
+				ref: 'User'
+			},
+			algorithm: {
+				type: Schema.Types.ObjectId,
+				ref: 'Algorithm'
+			},
+			strengthToUSD: Number,
+			encryptedPrivateKey: String,
+			worth: [
+				{
+					timestamp: Number,
+					value: Number
+				}
+			],
+			state: {
+				type: String, // paused, tempPaused, stopped, running
+				time: Number // used for tempPaused
 			}
-		]
-	},
-	{
-		virtuals: {
-			privateKey: {
-				get() {
-					// @TODO decrypt private key
-					return this.encryptedPrivateKey;
+		},
+		{
+			virtuals: {
+				privateKey: {
+					get() {
+						// @TODO decrypt private key
+						return this.encryptedPrivateKey;
+					}
 				}
 			}
 		}
-	}
-);
+	)
+};
 
-botSchema.pre('save', next => {
+schemas.Bot.pre('save', next => {
 	// @TODO encrypt private key
 	next();
 });
 
-export const Identity = models.Identity ?? model('Identity', identitySchema);
-export const User = models.User ?? model('User', userSchema);
-export const Algorithm = models.Algorithm ?? model('Algorithm', algorithmSchema);
-export const Bot = models.Bot ?? model('Bot', botSchema);
+const evaluateModel = name => models[name] ?? model(name, schemas[name]);
+
+// they are reassigned, but in an eval where eslint cant see them
+/* eslint-disable prefer-const */
+export let Identity = evaluateModel('Identity');
+export let User = evaluateModel('User');
+export let Algorithm = evaluateModel('Algorithm');
+export let Bot = evaluateModel('Bot');
+/* eslint-disable prefer-const */
+
+export const evaluateModelsWhenConnectionReady = async () => {
+	if (connection.readyState !== 1)
+		await new Promise(res => {
+			connection.on('connected', res);
+		});
+
+	Object.keys(schemas).forEach(name => {
+		// eslint-disable-next-line no-unused-vars
+		const modelEvaluation = evaluateModel(name);
+		// eslint-disable-next-line no-eval
+		eval(`${name} = modelEvaluation`);
+	});
+};
