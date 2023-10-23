acf6752451259dc44be4ba16b7e427be9f78c91f
CelestialCrafter <celestialcraftermc@gmail.com>
2023-10-23 14:08:02 -0500

    add default border to square buttons

--- src/routes/dashboard/BotControls.svelte
+++ src/routes/dashboard/BotControls.svelte
@@ -10,18 +10,20 @@
 	class="{$selectedAccount ? 'pointer-events-auto opacity-100' : 'pointer-events-none opacity-40'}
 	bg-section-200 flex flex-wrap items-center justify-center transition-[opacity] duration-300"
 >
-	<button class="square hover:!bg-white" title="Temporary Pause Bot"><Clock class="icon" /></button>
-	<button class="square border-green-500/20 hover:!bg-green-500" title="Resume Bot"
+	<button class="square !border-white/20 hover:!bg-white" title="Temporary Pause Bot"
+		><Clock class="icon" /></button
+	>
+	<button class="square !border-green-500/20 hover:!bg-green-500" title="Resume Bot"
 		><Play class="icon" />
 	</button>
-	<button class="square border-yellow-500/20 hover:!bg-yellow-500" title="Pause Bot"
+	<button class="square !border-yellow-500/20 hover:!bg-yellow-500" title="Pause Bot"
 		><Pause class="icon" />
 	</button>
-	<button class="square border-rose-500/20 hover:!bg-rose-500" title="Delete Bot">
+	<button class="square !border-rose-500/20 hover:!bg-rose-500" title="Delete Bot">
 		<Minus class="icon" />
 	</button>
-	<button class=" border-accent/20 square"><AdjustmentsHorizontal class="icon" /></button>
+	<button class="square"><AdjustmentsHorizontal class="icon" /></button>
 	{#if $user.admin}
-		<button class=" border-accent/20 square"><CommandLine class="icon" /></button>
+		<button class="square"><CommandLine class="icon" /></button>
 	{/if}
 </section>
