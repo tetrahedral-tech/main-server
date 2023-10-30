5da4880e2f34523e4493fc280bee937514db4c9c
CelestialCrafter <celestialcraftermc@gmail.com>
2023-10-30 12:28:16 -0500

    :/

--- src/lib/data.server.js
+++ src/lib/data.server.js
@@ -1,6 +1,8 @@
 import { Algorithm } from '$lib/models.server.js';
 
-export const getAllowedAlgorithms = id =>
-	Algorithm.find({
+export const getAllowedAlgorithms = async id => {
+	console.log(await Algorithm.find({}));
+	return Algorithm.find({
 		$or: [{ owner: id }, { owner: { $exists: false } }]
 	});
+};
