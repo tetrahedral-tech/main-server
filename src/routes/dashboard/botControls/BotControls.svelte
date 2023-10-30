287b1d5525b8c2a2fed83b23b1adddfd06f67c56
CelestialCrafter <celestialcraftermc@gmail.com>
2023-10-30 10:33:12 -0500

    add algorithm check force invoke

---
+++ src/routes/dashboard/botControls/BotControls.svelte
@@ -0,0 +1,46 @@
+<script>
+	import {
+		Pause,
+		Play,
+		Clock,
+		Minus,
+		AdjustmentsHorizontal,
+		CommandLine,
+		ArrowPath
+	} from 'svelte-heros-v2';
+	import { getContext } from 'svelte';
+
+	const selectedAccount = getContext('selectedAccount');
+	const user = getContext('user');
+</script>
+
+<section
+	class="{$selectedAccount ? 'pointer-events-auto opacity-100' : 'pointer-events-none opacity-40'}
+	bg-section-200 flex flex-wrap items-center justify-center transition-[opacity] duration-300"
+>
+	<form method="post">
+		<button class="square !border-white/20 hover:!bg-white" title="Temporary Pause Bot"
+			><Clock class="icon" /></button
+		>
+		<button class="square !border-green/20 hover:!bg-green" title="Resume Bot"
+			><Play class="icon" />
+		</button>
+		<button class="square !border-yellow/20 hover:!bg-yellow" title="Pause Bot"
+			><Pause class="icon" />
+		</button>
+		<button class="square !border-red/20 hover:!bg-red" title="Delete Bot">
+			<Minus class="icon" />
+		</button>
+		<button class="square"><AdjustmentsHorizontal class="icon" /></button>
+		{#if $user.admin}
+			<button class="square"><CommandLine class="icon" /></button>
+			<button class="square">
+				<ArrowPath
+					class="icon"
+					title="Invoke Algorithm Check"
+					formaction="/botControls?/invokeAlgorithmCheck"
+				/>
+			</button>
+		{/if}
+	</form>
+</section>
