9414c552747228067eaa8eb2a2e32b4287336d23
CelestialCrafter <celestialcraftermc@gmail.com>
2023-12-11 00:44:58 -0600

    fix lint errors again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,32 @@
-<main class="p-0 text-2xl overflow-visible" >
-    <div class="header absolute flex gap-3 items-center w-screen h-25 p-6 pl-64 pr-64 bg-section-300">
-        <img src="/notfound.png" class="h-16 rounded-md" alt="logo">
-        <p class="">auto-trader</p>
-        <span class="subtext">/</span>
-        <a href="/identity" class="text-xl transition-all underline-offset-3 decoration-white/30 hover:decoration-white underline hover:underline-offset-[6px]">sign-in</a>
-        <a href="/dashboard" class="text-xl transition-all underline-offset-3 decoration-white/30 hover:decoration-white underline hover:underline-offset-[6px]">dashboard</a>
-    </div>
+<main class="overflow-visible p-0 text-2xl">
+	<div class="header h-25 absolute flex w-screen items-center gap-3 bg-section-300 p-6 pl-64 pr-64">
+		<img src="/notfound.png" class="h-16 rounded-md" alt="logo" />
+		<p class="">auto-trader</p>
+		<span class="subtext">/</span>
+		<!-- eslint-disable max-len -->
+		<a
+			href="/identity"
+			class="underline-offset-3 text-xl underline decoration-white/30 transition-all hover:decoration-white hover:underline-offset-[6px]"
+			>sign-in</a
+		>
+		<a
+			href="/dashboard"
+			class="underline-offset-3 text-xl underline decoration-white/30 transition-all hover:decoration-white hover:underline-offset-[6px]"
+			>dashboard</a
+		>
+		<!-- eslint-enable max-len -->
+	</div>
 
-    <div class="flex w-screen h-screen justify-center items-center gap-3">
-        <div class="w-1/4">
-            <h1 class="text-6xl">lorem ipsum</h1>
-            <p class="subtext mt-4">dolor sit amet consectetur adipisicing elit. Explicabo corrupti, quibusdam laudantium sit, fugit nisi quae provident porro id laborum voluptatem perspiciatis praesentium, perferendis vero! Quo voluptas quod et ipsam.</p>
-        </div>
-        <img src="dashboard.webp" alt="dashboard" class="w-1/2 rounded-2xl">
-    </div>
+	<div class="flex h-screen w-screen items-center justify-center gap-3">
+		<div class="w-1/4">
+			<h1 class="text-6xl">lorem ipsum</h1>
+			<!-- eslint-disable-next-line max-len -->
+			<p class="subtext mt-4">
+				dolor sit amet consectetur adipisicing elit. Explicabo corrupti, quibusdam laudantium sit,
+				fugit nisi quae provident porro id laborum voluptatem perspiciatis praesentium, perferendis
+				vero! Quo voluptas quod et ipsam.
+			</p>
+		</div>
+		<img src="dashboard.webp" alt="dashboard" class="w-1/2 rounded-2xl" />
+	</div>
 </main>
